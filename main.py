0b763fe302edccbe1d2f1dea075fdea3b0e4c5a1
LeoCh01 <83481110+LeoCh01@users.noreply.github.com>
2022-11-14 21:21:21 -0500

    speedometer

---
+++ main.py
@@ -0,0 +1,51 @@
+import time, random, threading, math
+from dash import Dash, html, dcc, Input, Output
+import dash_daq as daq
+
+
+# def set_interval(func, sec):
+#     def func_wrapper():
+#         set_interval(func, sec)
+#         func()
+#     t = threading.Timer(sec, func_wrapper)
+#     t.start()
+#     return t
+
+
+app = Dash()
+app.layout = html.Div([
+    daq.Gauge(
+        id='gauge',
+        label='Speedometer',
+        min=0,
+        max=50,
+        value=6.4,
+        showCurrentValue=True,
+        units="MPH",
+        color="#1e81b0",
+        size=200,
+        scale={'start': 0, 'interval': 2, 'labelInterval': 5},
+        style={
+            'border': 'solid 2px #A2B1C6',
+            'border-radius': '5px',
+            'padding-top': '20px',
+            'margin': '50px',
+            'font': '2px Arial, Helvetica, sans-serif',
+            'width': '300px',
+        }
+    ),
+    dcc.Interval(
+        id='interval',
+        interval=100,
+        n_intervals=0,
+    ),
+])
+
+
+@app.callback(Output('gauge', 'value'), Input('interval', 'n_intervals'))
+def update_output(i):
+    return round(i * math.pi / 10, 2)
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
