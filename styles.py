fa71d1e615a44397f5c30513cefa464844102540
LeoCh01 <83481110+LeoCh01@users.noreply.github.com>
2022-12-20 15:29:01 -0500

    Add files via upload

---
+++ styles.py
@@ -0,0 +1,18 @@
+obj = {
+    'border': 'solid 2px #000',
+    'border-radius': '5px',
+    'padding': '10px',
+    'margin': '10px',
+    'font': '2px Arial, Helvetica, sans-serif',
+    'display': 'block',
+    'height': 'auto',
+}
+
+body = {
+    'display': 'flex',
+    'flex-flow': 'row wrap',
+    'justify-content': 'flex-start',
+    'border': 'solid 2px #000',
+    'border-radius': '5px',
+    'padding': '5px',
+}
